7a1272631f11083ff8a3b8b0c67022ce4391d820
Sharon Ji <you@example.com>
2015-09-18 16:53:20 -0700

    added create method

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -4,5 +4,21 @@ class CreaturesController < ApplicationController
 		render :index
 	end
 
+	def new
+		render :new
+	end
+
+	def create
+		new_creature = params.require(:creature).permit(:name, :description)
+
+		#take data from params and set new var, use params.require
+		
+		Creature.create(new_creature)
+		# create in database use Schema.create
+
+		# redirect back to collection view page
+		redirect_to "/creatures"
+	end
+
 
 end
